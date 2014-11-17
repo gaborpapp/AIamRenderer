e3fefe7ba23c5eb03c025d44a0029ce7151d2479
Gabor Papp <gabor.papp@gmail.com>
2014-11-17 16:05:33 +0100

    collecting avatar joints by bvh name

--- include/Avatar.h
+++ include/Avatar.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <string>
 
 #include "cinder/Filesystem.h"
 
@@ -20,5 +21,81 @@ class Avatar
  protected:
 	Avatar( const ci::fs::path &modelPath );
 
+	void collectJoints();
+
 	mndl::assimp::AssimpLoaderRef mAssimpLoader;
+
+	enum Joints
+	{
+		HIP = 0,
+		LOWER_SPINE,
+		MIDDLE_SPINE,
+		CHEST,
+		NECK,
+		HEAD,
+		HEAD_END,
+		LCLAVICLE,
+		LSHOULDER,
+		LFOREARM,
+		LHAND,
+		LFINGER1,
+		LFINGER11,
+		LFINGER12,
+		LFINGER12_END,
+		LFINGER2,
+		LFINGER21,
+		LFINGER22,
+		LFINGER22_END,
+		LFINGER3,
+		LFINGER31,
+		LFINGER32,
+		LFINGER32_END,
+		LFINGER4,
+		LFINGER41,
+		LFINGER42,
+		LFINGER42_END,
+		LFINGER0,
+		LFINGER01,
+		LFINGER02,
+		LFINGER02_END,
+		RCLAVICLE,
+		RSHOULDER,
+		RFOREARM,
+		RHAND,
+		RFINGER1,
+		RFINGER11,
+		RFINGER12,
+		RFINGER12_END,
+		RFINGER2,
+		RFINGER21,
+		RFINGER22,
+		RFINGER22_END,
+		RFINGER3,
+		RFINGER31,
+		RFINGER32,
+		RFINGER32_END,
+		RFINGER4,
+		RFINGER41,
+		RFINGER42,
+		RFINGER42_END,
+		RFINGER0,
+		RFINGER01,
+		RFINGER02,
+		RFINGER02_END,
+		RTHIGH,
+		RSHIN,
+		RFOOT,
+		RTOE,
+		RTOE_END,
+		LTHIGH,
+		LSHIN,
+		LFOOT,
+		LTOE,
+		LTOE_END,
+		TOTAL_JOINTS
+	};
+
+	mndl::assimp::AssimpNodeRef mJoints[ Joints::TOTAL_JOINTS ];
+
+	static std::string sJointNames[ Joints::TOTAL_JOINTS ];
 };
--- src/Avatar.cpp
+++ src/Avatar.cpp
@@ -1,3 +1,5 @@
+#include "cinder/app/App.h"
+
 #include "Avatar.h"
 
 using namespace ci;
@@ -5,4 +7,36 @@ using namespace ci;
 Avatar::Avatar( const fs::path &modelPath )
 {
 	mAssimpLoader = mndl::assimp::AssimpLoader::create( modelPath );
+
+	collectJoints();
 }
+
+void Avatar::collectJoints()
+{
+	for ( size_t i = 0; i < Joints::TOTAL_JOINTS; i++ )
+	{
+		const std::string &jointName = sJointNames[ i ];
+		auto node = mAssimpLoader->getAssimpNode( jointName );
+		mJoints[ i ] = node;
+		if ( ! node )
+		{
+			app::console() << "Warning: joint not found for name " << jointName << std::endl;
+		}
+	}
+}
+
+std::string Avatar::sJointNames[ Joints::TOTAL_JOINTS ] =
+{
+	"Hip", "LowerSpine", "MiddleSpine", "Chest", "Neck", "Head", "HeadEnd",
+	"LClavicle", "LShoulder", "LForearm", "LHand", "LFinger1", "LFinger11",
+	"LFinger12", "LFinger12End", "LFinger2", "LFinger21", "LFinger22",
+	"LFinger22End", "LFinger3", "LFinger31", "LFinger32", "LFinger32End",
+	"LFinger4", "LFinger41", "LFinger42", "LFinger42End", "LFinger0",
+	"LFinger01", "LFinger02", "LFinger02End", "RClavicle", "RShoulder",
+	"RForearm", "RHand", "RFinger1", "RFinger11", "RFinger12", "RFinger12End",
+	"RFinger2", "RFinger21", "RFinger22", "RFinger22End", "RFinger3",
+	"RFinger31", "RFinger32", "RFinger32End", "RFinger4", "RFinger41",
+	"RFinger42", "RFinger42End", "RFinger0", "RFinger01", "RFinger02",
+	"RFinger02End", "RThigh", "RShin", "RFoot", "RToe", "RToeEnd", "LThigh",
+	"LShin", "LFoot", "LToe", "LToeEnd"
+};
